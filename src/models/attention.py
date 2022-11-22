8a6ad8b4027dae00c251e170131a2bd8376adc58
daviddmc <junshen@mit.edu>
2022-11-22 11:22:22 -0500

    update attention

--- src/models/attention.py
+++ src/models/attention.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 import torchvision.models as tvm
 
 # modified from https://github.com/jadore801120/attention-is-all-you-need-pytorch
 class MultiHeadAttention(nn.Module):
-    ''' Multi-Head Attention module '''
+    """Multi-Head Attention module"""
 
     def __init__(self, n_head, d_model, d_k, d_v, dropout=0.0):
         super().__init__()
@@ -20,11 +19,13 @@ class MultiHeadAttention(nn.Module):
         self.w_vs = nn.Linear(d_model, n_head * d_v, bias=True)
         self.fc = nn.Linear(n_head * d_v, d_model, bias=True)
 
-        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5, dropout=dropout)
+        self.attention = ScaledDotProductAttention(
+            temperature=d_k**0.5, dropout=dropout
+        )
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
 
         x = x.unsqueeze(0)
 
@@ -46,10 +47,10 @@ class MultiHeadAttention(nn.Module):
         # Transpose for attention dot product: b x n x lq x dv
         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
 
-        #if mask is not None:
+        # if mask is not None:
         #    mask = mask.unsqueeze(1)   # For head axis broadcasting.
 
-        q, attn = self.attention(q, k, v)
+        q, attn = self.attention(q, k, v, mask)
 
         # Transpose to move the head dimension back: b x lq x n x dv
         # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
@@ -66,16 +67,20 @@ class MultiHeadAttention(nn.Module):
 
 
 class ScaledDotProductAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
+    """Scaled Dot-Product Attention"""
 
     def __init__(self, temperature, dropout=0.0):
         super().__init__()
         self.temperature = temperature
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, q, k, v):
+    def forward(self, q, k, v, neg_inf_mask=None):
         # b x n x lq x dv
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3)) #(b x n x lq x dv) (b x n x dv x lq)
+        attn = torch.matmul(
+            q / self.temperature, k.transpose(2, 3)
+        )  # (b x n x lq x dv) (b x n x dv x lq)
+        if neg_inf_mask is not None:
+            attn = attn + neg_inf_mask
         attn = F.softmax(attn, dim=-1)
         attn = self.dropout(attn)
         output = torch.matmul(attn, v)
@@ -84,12 +89,12 @@ class ScaledDotProductAttention(nn.Module):
 
 
 class PositionwiseFeedForward(nn.Module):
-    ''' A two-feed-forward-layer module '''
+    """A two-feed-forward-layer module"""
 
     def __init__(self, d_in, d_hid, dropout=0.0):
         super().__init__()
-        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
-        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.w_1 = nn.Linear(d_in, d_hid)  # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in)  # position-wise
         self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
         self.dropout = nn.Dropout(dropout)
 
@@ -107,21 +112,20 @@ class PositionwiseFeedForward(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    ''' Compose with two layers '''
+    """Compose with two layers"""
 
     def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
         super(EncoderLayer, self).__init__()
         self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout)
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout)
 
-    def forward(self, enc_input, slf_attn_mask=None):
-        enc_output, attn = self.slf_attn(enc_input)
+    def forward(self, enc_input, mask=None):
+        enc_output, attn = self.slf_attn(enc_input, mask)
         enc_output = self.pos_ffn(enc_output)
         return enc_output, attn
 
 
 class PositionalEncoding(nn.Module):
-
     def __init__(self, d_model, d_in):
         super().__init__()
         num_w = d_model // 2 // d_in
@@ -139,41 +143,48 @@ class PositionalEncoding(nn.Module):
 
 
 class TransformerEncoder(nn.Module):
-    ''' A encoder model with self attention mechanism. '''
+    """A encoder model with self attention mechanism."""
 
     def __init__(self, n_layers, n_head, d_k, d_v, d_model, d_inner, dropout):
 
         super().__init__()
 
         self.dropout = nn.Dropout(p=dropout)
-        self.layer_stack = nn.ModuleList([
-            EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
-            for _ in range(n_layers)])
+        self.layer_stack = nn.ModuleList(
+            [
+                EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+                for _ in range(n_layers)
+            ]
+        )
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
-    def forward(self, x, pos_enc):
+    def forward(self, x, pos_enc, mask=None):
 
         enc_output = self.dropout(x + pos_enc)
         enc_output = self.layer_norm(enc_output)
 
         for enc_layer in self.layer_stack:
-            enc_output, attn = enc_layer(enc_output)
+            enc_output, attn = enc_layer(enc_output, mask)
 
         return enc_output, attn
 
 
 class ResNet(nn.Module):
-
     def __init__(self, n_res, d_model, d_in=1, pretrained=False):
         super().__init__()
-        resnet_fn = getattr(tvm, 'resnet%d' % n_res)
-        model = resnet_fn(pretrained=pretrained, norm_layer=lambda x: nn.BatchNorm2d(x, track_running_stats=False))
+        resnet_fn = getattr(tvm, "resnet%d" % n_res)
+        model = resnet_fn(
+            pretrained=pretrained,
+            norm_layer=lambda x: nn.BatchNorm2d(x, track_running_stats=False),
+        )
         model.fc = nn.Linear(model.fc.in_features, d_model)
         if not pretrained:
-            model.conv1 = nn.Conv2d(d_in, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            model.conv1 = nn.Conv2d(
+                d_in, 64, kernel_size=7, stride=2, padding=3, bias=False
+            )
         self.model = model
         self.pretrained = pretrained
-        
+
     def forward(self, x):
         if self.pretrained:
             x = x.expand(-1, 3, -1, -1)
